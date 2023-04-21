42844eba600b2aa808aab3ea17258ab0bd83cbac
simon <simonkhartoum@gmail.com>
2023-04-21 12:48:03 +0300

    completed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,14 @@
 import React from "react";
-
+import Home from "../components/Home";
+import About from "../components/About";
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return (
+  <nav>
+   <a href="#home">{Home}</a>
+   <a href="#about">{About}</a>
+  </nav>
+  );
 }
 
 export default NavBar;
